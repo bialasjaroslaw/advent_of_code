45369a2e04659d574aa7f1bfbff9fc19115479ae
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-23 17:40:00 +0100

    Python 2015/05/2 improvements

--- 2015/python/05_2.py
+++ 2015/python/05_2.py
@@ -1,31 +1,17 @@
-import string
+import collections
+import itertools
 
-vowels = {'a', 'e', 'i', 'o', 'u'}
+def sliding_window(iterable, n):
+    it = iter(iterable)
+    window = collections.deque(itertools.islice(it, n-1), maxlen=n)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
 
 def is_nice_string(s: str):
-	three_seq = False
-	for idx in range(len(s) - 2):
-		if s[idx + 2] == s[idx]:
-			three_seq = True
-			break
-	if not three_seq:
-		return False
-
-	pair_seq = False
-	for idx in range(len(s) - 3):
-		pair = s[idx:idx + 2]
-		if s.find(pair, idx + 2) != -1:
-			pair_seq = True
-			break
-	if not pair_seq:
-		return False
-	return True
+	return any(map(lambda e: e[0] == e[2], sliding_window(s, 3))) and \
+		any(map(lambda pair: ''.join(pair[1]) in s[pair[0] + 2:], enumerate(sliding_window(s, 2))))
 
 
 with open('../input_05_1.txt', 'r') as f:
-	nice = 0
-	for line in f.readlines():
-		if is_nice_string(line):
-			nice += 1
-	print(nice)
-
+	print(sum(map(lambda line: is_nice_string(line), f.readlines())))
