3ac38bd402f6e376a8685e9b07789cd6feaec147
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-23 17:19:59 +0100

    Python 2015/05/1 improvements

--- 2015/python/05_1.py
+++ 2015/python/05_1.py
@@ -1,31 +1,21 @@
-import string
+import collections
+import itertools
 
 vowels = {'a', 'e', 'i', 'o', 'u'}
+bad_seq = {'ab', 'cd', 'pq', 'xy'}
+
+def sliding_window(iterable, n):
+    it = iter(iterable)
+    window = collections.deque(itertools.islice(it, n-1), maxlen=n)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
 
 def is_nice_string(s: str):
-	occurences = {char: s.count(char) for char in string.ascii_lowercase} 
-	sum = 0
-	for vowel in vowels:
-		sum += occurences[vowel]
-	if sum < 3:
-		return False
-	have_double = False
-	for idx in range(len(s) - 1):
-		if s[idx+1] == s[idx]:
-			have_double = True
-			break
-	if not have_double:
-		return False
-
-	if 'ab' in s or 'cd' in s or 'pq' in s or 'xy' in s:
-		return False
-	return True
+	return sum(map(lambda char: s.count(char), vowels)) >= 3 and \
+		not any(map(lambda x: x in s, bad_seq)) and \
+		any(map(lambda e: e[0] == e[1], sliding_window(s, 2)))
 
 
 with open('../input_05_1.txt', 'r') as f:
-	nice = 0
-	for line in f.readlines():
-		if is_nice_string(line):
-			nice += 1
-	print(nice)
-
+	print(sum(map(lambda line: is_nice_string(line), f.readlines())))
