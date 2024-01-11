a489dcfb25f70ded82416cdb37c80d17424b9164
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-01-11 20:19:40 +0100

    Improve Python 2015/08/1

--- 2015/python/08_1.py
+++ 2015/python/08_1.py
@@ -1,4 +1,4 @@
-def count_characters(line: str) -> int:
+def count_characters(line: str):
 	if len(line) < 2:
 		raise ValueError(f"Invalid string passed {line}")
 	line = line[1:-1]
@@ -22,12 +22,6 @@ def count_characters(line: str) -> int:
 		count += 1
 	return count
 
-
 with open('../input_08_1.txt', 'r') as f:
-	cumulative = 0
-	for line in f.read().split('\n'):
-		if line != "":
-			cumulative += len(line)
-			cumulative -= count_characters(line)
-	print(cumulative)
+	print(sum(len(line) - count_characters(line) for line in f.read().split('\n') if line != ""))
 
