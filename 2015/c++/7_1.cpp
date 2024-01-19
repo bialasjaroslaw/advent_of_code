cdd990b5252d3980e50bc2912409fa96f3050e03
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-01-19 22:41:09 +0100

    Improve C++ 2015/07/1 and 2015/07/2

--- 2015/c++/7_1.cpp
+++ 2015/c++/7_1.cpp
@@ -6,6 +6,8 @@
 #include <text_processing/sub.hpp>
 #include <text_processing/find.hpp>
 
+#include <range/v3/action/remove_if.hpp>
+
 #include <string_view>
 #include <unordered_map>
 
@@ -32,22 +34,88 @@ struct Relation{
 	bool l_stat = false;
 	bool r_stat = false;
 
-	bool set_l(int32_t val){
-		if(val < 0 || val > 65535)
-			return false;
-		l_stat = true;
-		l_value = static_cast<uint16_t>(val);
-		if(op == Operation::Equal){
-			op = Operation::Solved;
+
+	static int32_t convert_or_neg(auto str){
+		try{
+			return Text::convert<int32_t>(str);
 		}
-		return true;
+		catch(...){
+			return -1;
+		}
+	}
+
+	static Relation create(const std::string& receipe){
+		auto parts = Text::split(receipe, " -> ");
+		if(parts.size() != 2)
+			throw std::runtime_error("Invalid size");
+		Relation rp;
+		rp.result = parts[1];
+		if(Text::find(parts[0], "NOT ") != Text::End){
+			rp.op = Operation::Not;
+			rp.lhs = Text::mid(parts[0], 4);
+		}
+		else if(Text::find(parts[0], " AND ") != Text::End){
+			auto operands = Text::split(parts[0], " AND ");
+			rp.op = Operation::And;
+			if(!rp.set_l(convert_or_neg(operands[0]))){
+				rp.lhs = operands[0];
+			}
+			if(!rp.set_r(convert_or_neg(operands[1]))){
+				rp.rhs = operands[1];	
+			}
+		}
+		else if(Text::find(parts[0], " OR ") != Text::End){
+			auto operands = Text::split(parts[0], " OR ");
+			rp.op = Operation::Or;
+			if(!rp.set_l(convert_or_neg(operands[0]))){
+				rp.lhs = operands[0];
+			}
+			if(!rp.set_r(convert_or_neg(operands[1]))){
+				rp.rhs = operands[1];	
+			}
+		}
+		else if(Text::find(parts[0], " LSHIFT ") != Text::End){
+			auto operands = Text::split(parts[0], " LSHIFT ");
+			rp.op = Operation::LShift;
+			rp.lhs = operands[0];
+			rp.set_r(convert_or_neg(operands[1]));
+		}
+		else if(Text::find(parts[0], " RSHIFT ") != Text::End){
+			auto operands = Text::split(parts[0], " RSHIFT ");
+			rp.op = Operation::RShift;
+			rp.lhs = operands[0];
+			rp.set_r(convert_or_neg(operands[1]));
+		}
+		else{
+			rp.op = Operation::Equal;
+			auto val = convert_or_neg(parts[0]);
+			if(val >= 0)
+				rp.set_l(val);
+			rp.lhs = parts[0];
+		}
+		return rp;
+	}
+
+	static bool validate_value(int32_t value){
+		return value >= 0 && value <= 65535;
+	}
+
+	bool set_l(int32_t val){
+		if(Relation::validate_value(val)){
+			l_stat = true;
+			l_value = static_cast<uint16_t>(val);
+			if(op == Operation::Equal){
+				op = Operation::Solved;
+			}
+		}
+		return l_stat;
 	}
 	bool set_r(int32_t val){
-		if(val < 0 || val > 65535)
-			return false;
-		r_stat = true;
-		r_value = static_cast<uint16_t>(val);
-		return true;
+		if(Relation::validate_value(val)){
+			r_stat = true;
+			r_value = static_cast<uint16_t>(val);
+		}
+		return r_stat;
 	}
 
 	bool validate_l(){
@@ -66,6 +134,12 @@ struct Relation{
 			r_value = solved[rhs];
 		}
 		return r_stat;
+	}
+
+	bool validate(){
+		auto lhs_solvable = validate_l();
+		auto rhs_solvable = validate_r();
+		return lhs_solvable && rhs_solvable;
 	}
 
 	uint16_t solve(){
@@ -94,83 +168,6 @@ struct Relation{
 };
 
 std::vector<Relation> ripple;
-std::unordered_map<Operation, std::string> op_to_str{
-	{Operation::Equal, "Equal"},
-	{Operation::Not, "Not"},
-	{Operation::And, "And"},
-	{Operation::Or, "Or"},
-	{Operation::LShift, "LShift"},
-	{Operation::RShift, "RShift"},
-	{Operation::Solved, "Solved"},
-	{Operation::Invalid, "Invalid"},
-};
-
-int32_t convert_or_neg(auto str){
-	try{
-		return Text::convert<int32_t>(str);
-	}
-	catch(...){
-		return -1;
-	}
-}
-
-Relation make_relation(const std::string& receipe){
-	auto parts = Text::split(receipe, " -> ");
-	if(parts.size() != 2)
-		throw std::runtime_error("Invalid size");
-	Relation rp;
-	rp.result = parts[1];
-	if(Text::find(parts[0], "NOT ") != Text::End){
-		rp.op = Operation::Not;
-		rp.lhs = Text::mid(parts[0], 4);
-	}
-	else if(Text::find(parts[0], " AND ") != Text::End){
-		auto operands = Text::split(parts[0], " AND ");
-		rp.op = Operation::And;
-		if(!rp.set_l(convert_or_neg(operands[0]))){
-			rp.lhs = operands[0];
-		}
-		if(!rp.set_r(convert_or_neg(operands[1]))){
-			rp.rhs = operands[1];	
-		}
-	}
-	else if(Text::find(parts[0], " OR ") != Text::End){
-		auto operands = Text::split(parts[0], " OR ");
-		rp.op = Operation::Or;
-		if(!rp.set_l(convert_or_neg(operands[0]))){
-			rp.lhs = operands[0];
-		}
-		if(!rp.set_r(convert_or_neg(operands[1]))){
-			rp.rhs = operands[1];	
-		}
-	}
-	else if(Text::find(parts[0], " LSHIFT ") != Text::End){
-		auto operands = Text::split(parts[0], " LSHIFT ");
-		rp.op = Operation::LShift;
-		rp.lhs = operands[0];
-		rp.set_r(convert_or_neg(operands[1]));
-	}
-	else if(Text::find(parts[0], " RSHIFT ") != Text::End){
-		auto operands = Text::split(parts[0], " RSHIFT ");
-		rp.op = Operation::RShift;
-		rp.lhs = operands[0];
-		rp.set_r(convert_or_neg(operands[1]));
-	}
-	else{
-		rp.op = Operation::Equal;
-		auto val = convert_or_neg(parts[0]);
-		if(val >= 0)
-			rp.set_l(val);
-		rp.lhs = parts[0];
-	}
-	return rp;
-}
-
-bool validate(Relation& rel){
-	auto lhs_solvable = rel.validate_l();
-	auto rhs_solvable = rel.validate_r();
-	return lhs_solvable && rhs_solvable;
-}
 
 int main(int argc, char const *argv[])
 {
@@ -192,7 +189,7 @@ int main(int argc, char const *argv[])
 	for(const auto& line : lines){
 		if(line.size() == 0)
 			continue;
-		auto relation = make_relation(line);
+		auto relation = Relation::create(line);
 		if(relation.op == Operation::Solved)
 			solved[relation.result] = relation.l_value;
 		else{
@@ -204,20 +201,13 @@ int main(int argc, char const *argv[])
 	while(changed){
 		changed = false;
 		for(auto& ri : ripple){
-			if(validate(ri)){
+			if(ri.validate()){
 				solved[ri.result] = ri.solve();
 				changed = true;
 			}
 		}
-		ripple.erase(std::remove_if(ripple.begin(), ripple.end(), [](const Relation& r){
-			return r.op == Operation::Solved;
-		}), ripple.end());
+		ripple = ranges::actions::remove_if(ripple, [](const Relation& r){ return r.op == Operation::Solved; });
 	}
 	fmt::print("{}\n", solved["a"]);
 	return 0;
-
-	// while(ripple.size() > 0){
-
-	// }
-	// return 0;
 }
