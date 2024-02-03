b4a63982e9de3045e108f73c82d52b4fa6c14612
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-02-03 11:34:35 +0100

    Python 2015/17/2

---
+++ 2015/python/17_2.py
@@ -0,0 +1,11 @@
+import itertools
+with open('../input_17_1.txt') as file:
+    containers = [int(line.strip()) for line in file]
+total = 0
+for num in range(len(containers)):
+    for combination in itertools.combinations(containers, num):
+        if sum(combination) == 150:
+            total += 1
+    if total != 0:
+        break
+print(total)
