f92ab07cb90896f563275cb25d5da58bc7fb7843
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-02-04 11:41:50 +0100

    Python 2015/17/1 and 2015/17/2
    Add notes

--- 2015/python/17_1.py
+++ 2015/python/17_1.py
@@ -1,9 +1,16 @@
 import itertools
+
+def next_sum(container):
+    elem_sum = 0
+    for idx, elem in enumerate(container):
+        elem_sum += elem
+        yield idx, elem_sum
+    raise StopIteration()
+
 with open('../input_17_1.txt') as file:
     containers = [int(line.strip()) for line in file]
-total = 0
-for num in range(len(containers)):
-    for combination in itertools.combinations(containers, num):
-        if sum(combination) == 150:
-            total += 1
-print(total)
+
+containers.sort()
+lower_bound = next((idx for idx, value in next_sum(reversed(containers)) if value >= 150), None)
+upper_bound = next((idx for idx, value in next_sum(containers) if value > 150), None) + 2
+print(sum(sum(combination) == 150 for num in range(lower_bound, upper_bound) for combination in itertools.combinations(containers, num)))
--- 2015/python/17_2.py
+++ 2015/python/17_2.py
@@ -1,11 +1,16 @@
 import itertools
+
+def next_sum(container):
+    elem_sum = 0
+    for idx, elem in enumerate(container):
+        elem_sum += elem
+        yield idx, elem_sum
+    raise StopIteration()
+
 with open('../input_17_1.txt') as file:
     containers = [int(line.strip()) for line in file]
-total = 0
-for num in range(len(containers)):
-    for combination in itertools.combinations(containers, num):
-        if sum(combination) == 150:
-            total += 1
-    if total != 0:
-        break
-print(total)
+
+containers.sort()
+bound = next((idx for idx, value in next_sum(containers) if value >= 150), None)
+min_num = next((num for num in range(bound) for combination in itertools.combinations(containers, num) if sum(combination) == 150), None)
+print(sum(sum(combination) == 150 for combination in itertools.combinations(containers, min_num)))
